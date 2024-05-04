5b1473780f6356a9480e5238f139e2ee1cfad706
Hieu Pham <trunghieupham03@gmail.com>
2024-05-04 22:28:15 +0700

    fix: response

--- utils/response.py
+++ utils/response.py
@@ -17,5 +17,15 @@ def response_with_message(message="200 Success", status_code=200):
 def response_with_error(file, message="400 Bad Request", status_code=400, error=None):
     if error:
         log_prefix(file, error, type="error")
+        return (
+            jsonify(
+                {
+                    "success": False,
+                    "message": message,
+                    "error": str(error),
+                }
+            ),
+            status_code,
+        )
 
     return jsonify({"success": False, "message": message}), status_code
