6ada764fa8c15afb0b200628f6e020cd7f2e213e
Thanh Vinh <ptvinh203@gmail.com>
2024-06-16 12:09:46 +0700

    feat: vietnamese data

--- seed/user.py
+++ seed/user.py
@@ -31,28 +31,34 @@ def user_seeder(repeat_times=1000, reset=False):
         if not USER_ROLE:
             raise Exception("User role not found")
 
-        for _ in range(repeat_times):
-            account = Account(
-                address=fake.address(),
-                email=fake.email(),
-                password=Env.DEFAULT_PASSWORD,
-                phone_number=fake_phone_numbers(),
-                refresh_token=fake.password(),
-                system_role=USER_ROLE.constant_id,
-                avatar=image_urls[_ % len(image_urls)] if image_urls else "",
-            )
+        accounts = Account.query.filter_by(system_role=USER_ROLE.constant_id).all()
+
+        for idx, account in enumerate(accounts):
+            account.avatar = image_urls[idx % len(image_urls)] if image_urls else ""
             db.session.add(account)
 
-            user = User(
-                account_id=account.account_id,
-                date_of_birth=fake.date_of_birth(maximum_age=40, minimum_age=18),
-                first_name=fake.first_name(),
-                gender=fake.boolean(),
-                last_name=fake.last_name(),
-                social_media_link=[fake.domain_name() for _ in range(randint(0, 10))],
-                summary_introduction=fake.text(),
-            )
-            db.session.add(user)
+        # for _ in range(repeat_times):
+            # account = Account(
+            #     address=fake.address(),
+            #     email=fake.email(),
+            #     password=Env.DEFAULT_PASSWORD,
+            #     phone_number=fake_phone_numbers(),
+            #     refresh_token=fake.password(),
+            #     system_role=USER_ROLE.constant_id,
+            #     avatar=image_urls[_ % len(image_urls)] if image_urls else "",
+            # )
+            # db.session.add(account)
+
+            # user = User(
+            #     account_id=account.account_id,
+            #     date_of_birth=fake.date_of_birth(maximum_age=40, minimum_age=18),
+            #     first_name=fake.first_name(),
+            #     gender=fake.boolean(),
+            #     last_name=fake.last_name(),
+            #     social_media_link=[fake.domain_name() for _ in range(randint(0, 10))],
+            #     summary_introduction=fake.text(),
+            # )
+            # db.session.add(user)
 
         db.session.commit()
         logger.info("Finished seeding Users...")
